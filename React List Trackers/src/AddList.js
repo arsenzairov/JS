9dd4a9a541d41c4c396345facc7fb8751de3e153
Arsen Zairov <30402084+arsenzairov@users.noreply.github.com>
2018-01-22 23:41:56 -0500

    Add files via upload

---
+++ React List Trackers/src/AddList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class AddList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            listName : ''
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault(); // this prevents the page from reloading -- do not delete this line!
+
+        var value = this.refs.id.value;
+
+        if (value === '') {
+            alert('Name is required');
+        }
+        else {
+            this.setState({
+                listName : value
+            } , function() {
+                this.props.addList(this.state);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div id="addListDiv">
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div id='addList'>
+                        <label>What will be on your next list?&nbsp;
+                            <input type='text' ref='id' id='newID'></input>
+                        </label>
+                    </div><br />
+                    <input type='submit' value='Create List' />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddList;
